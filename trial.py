1447dcf04065d087cf2066307f571b8f080bdf9e
Charis Chakim <132967245+CharisChakim@users.noreply.github.com>
2024-03-13 10:09:42 +0700

    Commit perubahan sebelum merge

--- trial.py
+++ trial.py
@@ -1,87 +1,102 @@
-from selenium import webdriver
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
+# from selenium import webdriver
+# from selenium.webdriver.common.by import By
+# from selenium.webdriver.support.ui import WebDriverWait
+# from selenium.webdriver.support import expected_conditions as EC
 
-# Mendapatkan konten HTML dari halaman web
-url = "https://www.cermati.com/karir"
-driver = webdriver.Chrome()
+# # Mendapatkan konten HTML dari halaman web
+# url = "https://www.cermati.com/karir"
+# driver = webdriver.Chrome()
 
-# Open web
-driver.get(url)
+# # Open web
+# driver.get(url)
 
-# Klik tombol "View All Jobs"
-view_all_jobs_button = WebDriverWait(driver, 10).until(
-    EC.presence_of_element_located((By.XPATH, "//a[contains(text(), 'View All Jobs')]"))
-)
-view_all_jobs_button.click()
+# # Klik tombol "View All Jobs"
+# view_all_jobs_button = WebDriverWait(driver, 10).until(
+#     EC.presence_of_element_located((By.XPATH, "//a[contains(text(), 'View All Jobs')]"))
+# )
+# view_all_jobs_button.click()
 
-# Function to scrape job data
-def scrape_job_data():
-    # Find all Apply buttons
-    apply_buttons = WebDriverWait(driver, 10).until(
-        EC.presence_of_all_elements_located((By.XPATH, "//a[@class='btn btn-action btn-lg a-btn']"))
-    )
+# # Function to scrape job data
+# def scrape_job_data():
+#     # Find all Apply buttons
+#     apply_buttons = WebDriverWait(driver, 10).until(
+#         EC.presence_of_all_elements_located((By.XPATH, "//a[@class='btn btn-action btn-lg a-btn']"))
+#     )
 
-    # Iterate through each Apply button
-    for apply_button in apply_buttons:
-        # Click the Apply button to open the job page
-        apply_button.click()
+#     # Iterate through each Apply button
+#     for apply_button in apply_buttons:
+#         # Click the Apply button to open the job page
+#         apply_button.click()
         
-        # Switch to the new window/tab
-        driver.switch_to.window(driver.window_handles[1])
+#         # Switch to the new window/tab
+#         driver.switch_to.window(driver.window_handles[1])
         
-        # Here you can scrape data from the new page
-        # For example, you can find elements by XPath and extract text
-        # Example:
-        job_title = WebDriverWait(driver, 10).until(
-            EC.presence_of_element_located((By.XPATH, "/html/body/div[3]/div/div/div[1]/main/h1"))
-        )
-        job_location = WebDriverWait(driver, 10).until(
-            EC.presence_of_element_located((By.XPATH, "/html/body/div[3]/div/div/div[1]/main/ul/li[1]/span/spl-job-location"))
-        )
-        job_desc = WebDriverWait(driver, 10).until(
-            EC.presence_of_element_located((By.XPATH, '//*[@id="st-jobDescription"]/div[2]'))
-        )
-        job_q = WebDriverWait(driver, 10).until(
-            EC.presence_of_element_located((By.XPATH, '//*[@id="st-qualifications"]/div[2]'))
-        )
+#         # Here you can scrape data from the new page
+#         # For example, you can find elements by XPath and extract text
+#         # Example:
+#         job_title = WebDriverWait(driver, 10).until(
+#             EC.presence_of_element_located((By.XPATH, "/html/body/div[3]/div/div/div[1]/main/h1"))
+#         )
+#         job_location = WebDriverWait(driver, 10).until(
+#             EC.presence_of_element_located((By.XPATH, "/html/body/div[3]/div/div/div[1]/main/ul/li[1]/span/spl-job-location"))
+#         )
+#         job_desc = WebDriverWait(driver, 10).until(
+#             EC.presence_of_element_located((By.XPATH, '//*[@id="st-jobDescription"]/div[2]'))
+#         )
+#         job_q = WebDriverWait(driver, 10).until(
+#             EC.presence_of_element_located((By.XPATH, '//*[@id="st-qualifications"]/div[2]'))
+#         )
        
-        job_title = job_title.text
-        job_location = job_location.text
-        job_desc = job_desc.text
-        job_q = job_q.text
+#         job_title = job_title.text
+#         job_location = job_location.text
+#         job_desc = job_desc.text
+#         job_q = job_q.text
         
-        print('Job Title: {job_title}\nLocation: {job_location}\nDesc: {job_desc}\nQ: {job_q}'.format(job_title=job_title, job_location=job_location, job_desc=job_desc, job_q=job_q))
+#         print('Job Title: {job_title}\nLocation: {job_location}\nDesc: {job_desc}\nQ: {job_q}'.format(job_title=job_title, job_location=job_location, job_desc=job_desc, job_q=job_q))
 
 
-        # Close the new window/tab and switch back to the original one
-        driver.close()
-        driver.switch_to.window(driver.window_handles[0])
+#         # Close the new window/tab and switch back to the original one
+#         driver.close()
+#         driver.switch_to.window(driver.window_handles[0])
 
-# Function to click the arrow button and scrape job data
-def click_arrow_and_scrape():
-    while True:
-        # Find the arrow button
-        arrow_button = WebDriverWait(driver, 10).until(
-            EC.presence_of_element_located((By.CSS_SELECTOR, "button.arrow-icon"))
-        )
+# # Function to click the arrow button and scrape job data
+# def click_arrow_and_scrape():
+#     while True:
+#         # Find the arrow button
+#         arrow_button = WebDriverWait(driver, 10).until(
+#             EC.presence_of_element_located((By.CSS_SELECTOR, "button.arrow-icon"))
+#         )
         
-        # Click the arrow button
-        arrow_button.click()
+#         # Click the arrow button
+#         arrow_button.click()
         
-        # Scrape job data
-        scrape_job_data()
+#         # Scrape job data
+#         scrape_job_data()
         
-        # Check if the arrow button is disabled (reached the end)
-        arrow_disabled = WebDriverWait(driver, 10).until(
-            EC.presence_of_element_located((By.CSS_SELECTOR, "button.arrow-icon.disabled"))
-        )
-        if arrow_disabled:
-            break
+#         # Check if the arrow button is disabled (reached the end)
+#         arrow_disabled = WebDriverWait(driver, 10).until(
+#             EC.presence_of_element_located((By.CSS_SELECTOR, "button.arrow-icon.disabled"))
+#         )
+#         if arrow_disabled:
+#             break
 
-# Start the process
-click_arrow_and_scrape()
+# # Start the process
+# click_arrow_and_scrape()
 
-# Close the browser
-driver.quit()
+# # Close the browser
+# driver.quit()
+
+import requests as req
+from bs4 import BeautifulSoup as bs
+import pandas as pd
+
+url = f'https://books.toscrape.com/catalogue/page-1.html'
+respone = req.get(url)
+    # print(respone)
+respone = respone.content
+    # print(respone)
+
+soup = bs(respone,'html.parser')
+order_list = soup.find('ol')
+print(order_list)
+articles = order_list.find_all('article', class_= 'product_pod')
--- yellow-pages/ws-yellow_page.py
+++ yellow-pages/ws-yellow_page.py
@@ -6,7 +6,6 @@ import csv
 # location = input('please enter the location too :')
 key = 'hotel'
 location = 'london'
-# url = 'https://www.yell.com/ucs/UcsSearchAction.do?scrambleSeed=1673388186&keywords={}&location={}&pageNum='.format(key, location)
 url = 'https://www.yell.com/ucs/UcsSearchAction.do?keywords={}&location={}&scrambleSeed=1640810789&pageNum='.format(key, location)
 headers = {
     'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/84.0.4147.125 Safari/537.36'
